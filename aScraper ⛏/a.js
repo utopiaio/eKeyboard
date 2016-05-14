caeeaf2b58c10e0a325c0afe785f74388dee73a1
Moe Szyslak <moe.duffdude@gmail.com>
2016-05-14 11:36:53 +0300

    optimization
    - fully generic (publication level) and configurable, web and direct
    - performance optimizations

--- aScraper ⛏/a.js
+++ aScraper ⛏/a.js
@@ -3,121 +3,62 @@ const cheerio = require('cheerio');
 const _ = require('lodash');
 
 /**
- * given an article url it'll extract words, no numbers or single letters words.
+ * given a url | utf8 content it'll extract Amharic words
+ * It'll not extract single letter words or digits.
  *
- * @param  {String}   url
+ * usage:
+ * (url, {...}) - web mode
+ * (url)        - web mode
+ * (utf8, null) - extract from passed text
+ *
+ * @param  {String}   url | utf8 content
  * @param  {Object}   options
  * @param  {Boolean}  options.unique
  * @param  {Boolean}  options.frequency
  * @return {Promise}
  */
-const contentWords = (url, options = { selector: 'body', unique: false, frequency: true }) => {
-  // I'm not going to force you to use node-v6+...not yet 😎...i lied
-  Object.assign(options, { selector: 'body', unique: false, frequency: true }, options);
-
+module.exports = (url, options = { selector: 'body', unique: false, frequency: true }) => {
   const promise = new Promise((resolve, reject) => {
-    request
-      .get(encodeURI(decodeURI(url)))
-      .end((err, res) => {
-        if (err || !res.ok) {
-          reject(err);
+    const extract = (content) => {
+      let words = Object.assign([], content.match(/[\u1200-\u135A]{2,}/g));
+
+      if (options.unique === true) {
+        words = _.uniq(words);
+      }
+
+      if (options.frequency === true) {
+        if (options.frequency === true) {
+          const frequency = {};
+
+          words.forEach((word) => {
+            frequency[word] = frequency.hasOwnProperty(word) === true ? ++frequency[word] : 1;
+          });
+
+          resolve(frequency);
         } else {
-          let allWords = cheerio.load(res.text)(options.selector)
-                                .text()
-                                .match(/[\u1200-\u135A]{2,}/g); // ignoring single letter words
+          resolve(words);
+        }
+      }
+    };
 
-          if (options.unique === true) {
-            allWords = _.uniq(allWords);
-          }
+    if (options === null) {
+      extract(url);
+    } else if (typeof options === 'object') { // yes I know the "quirks" or `typeof`
+      Object.assign({ selector: 'body', unique: false, frequency: true }, options);
 
-          if (options.frequency === true) {
-            const frequency = {};
-
-            allWords.forEach((word) => {
-              frequency[word] = frequency.hasOwnProperty(word) === true ? ++frequency[word] : 1;
-            });
-
-            resolve(frequency);
+      request
+        .get(encodeURI(decodeURI(url))) // makes sure we don't run into double-encoding scenario
+        .end((err, res) => {
+          if (err || !res.ok) {
+            reject(err);
           } else {
-            resolve(allWords);
+            extract(cheerio.load(res.text)(options.selector).text());
           }
-        }
-
-        return 'Thou shall not scrape without permission';
-      });
+        });
+    } else {
+      reject('Invalid options passed');
+    }
   });
 
   return promise;
-};
-
-/**
- * given a tag url of a tag it'll return all page links
- *
- * @param {String} url
- * @return {Promise}
- */
-const tagPageLinks = (url) => {
-  const promise = new Promise((resolve, reject) => {
-    request
-      .get(encodeURI(decodeURI(url)))
-      .end((err, res) => {
-        if (err || !res.ok) {
-          reject(err);
-        } else {
-          const $ = cheerio.load(res.text);
-          let lastPage = Number($('.pagination .pager-last a').attr('href').match(/page=(\d+)/)[1]);
-
-          if (Number.isNaN(lastPage) === true) {
-            reject('Could not find last page');
-          } else {
-            const contentLinks = [];
-
-            while (lastPage > 0) {
-              contentLinks.push(`${url}?page=${lastPage}`);
-              --lastPage;
-            }
-
-            resolve(contentLinks);
-          }
-        }
-      });
-  });
-
-  return promise;
-};
-
-/**
- * given tag page it'll extract links to article content
- *
- * @param {String} url
- * @param {String} base url to be used if the link extracted starts with `/`
- * @return {Promise}
- */
-const contentLinks = (url, base = '') => {
-  const promise = new Promise((resolve, reject) => {
-    request
-      .get(encodeURI(decodeURI(url)))
-      .end((err, res) => {
-        if (err || !res.ok) {
-          reject(err);
-        } else {
-          const links = cheerio.load(res.text)('#page-main-content article .post-title a');
-
-          // cheerio has `map` which is acting weird, it's returning holes (i think)
-          // so i had to fall-back to the inevitable _
-          resolve(_.map(links, link => (link.attribs.href.startsWith('/') === true
-            ? `${base}${link.attribs.href}`
-            : link.attribs.href
-          )));
-        }
-      });
-  });
-
-  return promise;
-};
-
-module.exports = {
-  tagPageLinks,
-  contentLinks,
-  contentWords,
 };

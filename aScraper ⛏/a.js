74d81899666df2532a13eebf855f72944d071bbf
Moe Szyslak <moe.duffdude@gmail.com>
2016-05-14 11:46:38 +0300

    refactor leftovers 😋

--- aScraper ⛏/a.js
+++ aScraper ⛏/a.js
@@ -8,16 +8,18 @@ const _ = require('lodash');
  *
  * usage:
  * (url, {...}) - web mode
- * (url)        - web mode
- * (utf8, null) - extract from passed text
+ * (utf8, { selector: null }) - extract from passed text
  *
  * @param  {String}   url | utf8 content
  * @param  {Object}   options
+ * @param  {String}   options.selector
  * @param  {Boolean}  options.unique
  * @param  {Boolean}  options.frequency
  * @return {Promise}
  */
 module.exports = (url, options = { selector: 'body', unique: false, frequency: true }) => {
+  Object.assign({ selector: 'body', unique: false, frequency: true }, options);
+
   const promise = new Promise((resolve, reject) => {
     const extract = (content) => {
       let words = Object.assign([], content.match(/[\u1200-\u135A]{2,}/g));
@@ -27,25 +29,21 @@ module.exports = (url, options = { selector: 'body', unique: false, frequency: t
       }
 
       if (options.frequency === true) {
-        if (options.frequency === true) {
-          const frequency = {};
+        const frequency = {};
 
-          words.forEach((word) => {
-            frequency[word] = frequency.hasOwnProperty(word) === true ? ++frequency[word] : 1;
-          });
+        words.forEach((word) => {
+          frequency[word] = frequency.hasOwnProperty(word) === true ? ++frequency[word] : 1;
+        });
 
-          resolve(frequency);
-        } else {
-          resolve(words);
-        }
+        resolve(frequency);
+      } else {
+        resolve(words);
       }
     };
 
-    if (options === null) {
+    if (options.selector === null) {
       extract(url);
-    } else if (typeof options === 'object') { // yes I know the "quirks" or `typeof`
-      Object.assign({ selector: 'body', unique: false, frequency: true }, options);
-
+    } else {
       request
         .get(encodeURI(decodeURI(url))) // makes sure we don't run into double-encoding scenario
         .end((err, res) => {
@@ -55,8 +53,6 @@ module.exports = (url, options = { selector: 'body', unique: false, frequency: t
             extract(cheerio.load(res.text)(options.selector).text());
           }
         });
-    } else {
-      reject('Invalid options passed');
     }
   });
 
